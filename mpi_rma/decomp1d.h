00a5bd1a44d9ca7ff8552873effc1aa2479263d8
LinLi-max <83507697+LinLi-max@users.noreply.github.com>
2021-08-25 23:33:49 +0800

    Delete mpi_rma directory

--- mpi_rma/decomp1d.h
+++
@@ -1,3 +0,0 @@
-#include "grid.h"
-
-int decomp1d(int rank, int size, int *s, int *e);
--- mpi_rma/init.h
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-#include "grid.h"
-
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
-void cfl(double ***U, double dx, double dy, double* dt, double sf, double gam);
-void init(double ***U, double dx, double dy, double gam, int rank, int s, int e);
-void bound(double ***U, double dx, double dy, double gam, int s, int e, int rank, int size);
