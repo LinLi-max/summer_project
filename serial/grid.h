6d9fd53d6a302de400886bdfc2126de16421e8eb
LinLi-max <83507697+LinLi-max@users.noreply.github.com>
2021-08-25 23:34:05 +0800

    Delete serial directory

--- serial/grid.h
+++
@@ -1,5 +0,0 @@
-// #define gx 1600
-// #define gy 400
-
-#define gx 400
-#define gy 100
--- serial/solve.h
+++
@@ -1,5 +0,0 @@
-#include "init.h"
-
-void solver_2d(double ***U, double ***U_half, double ***F, double ***G, double ***temp, double dx, double dy, double dt, double gam);
-void solve_x(double ***U, double ***U_half, double ***F, double ***temp, double dx, double dt, double gam);
-void solve_y(double ***U, double ***U_half, double ***G, double ***temp, double dy, double dt, double gam);
